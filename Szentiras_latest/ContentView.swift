5568f7723dd2fcd3e1636e3494907a69df10fd1e
Gabor Sornyei <info@sornyei.com>
2023-02-16 17:16:27 +0100

    Initial Commit

---
+++ Szentiras_latest/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Szentiras_latest
+//
+//  Created by Gabor Sornyei on 2023. 02. 16..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Szentiras_latest/Szentiras_latestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Szentiras_latestApp.swift
+//  Szentiras_latest
+//
+//  Created by Gabor Sornyei on 2023. 02. 16..
+//
+
+import SwiftUI
+
+@main
+struct Szentiras_latestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
